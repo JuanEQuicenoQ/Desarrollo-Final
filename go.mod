ad94f6c13e85269e51dcdc12300ab13510635a03
Juan Quiceno <doneprincipios@gmail.com>
2023-12-02 22:29:49 -0500

    Ajustes 10:30pm

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
-module github.com
+module github.com/JuanEQuicenoQ/Desarrollo-Final
 
 go 1.21.4
+
+require github.com/gorilla/mux v1.8.1
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
---
+++ models/libro.go
@@ -0,0 +1,11 @@
+package models
+
+type Libro struct {
+	Id          int    `db:"id" json:"id"`
+	Titulo      string `db:"titulo" json:"titulo"`
+	Autor       string `db:"autor" json:"autor"`
+	Edicion     uint   `db:"edicion" json:"edicion"`
+	Pais        string `db:"pais" json:"pais"`
+	Publicacion uint   `db:"publicacion" json:"publicacion"`
+	BestSeller  bool   `db:"bestseller" json:"bestseller"`
+}
